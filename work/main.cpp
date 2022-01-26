7a58068f523f8f65cfa01556c5d5fa6993fea4f8
mlotti <mikko.lotti@cern.ch>
2022-01-26 16:08:46 +0100

    renamed include directory to interface

--- work/main.cpp
+++ work/main.cpp
@@ -6,37 +6,37 @@
 
 int main()
 {
-    std::map<std::string, int> result;
+  std::map<std::string, int> result;
 
-    Text text;
-    Test test;
+  Text text;
+  Test test;
 
-    if (1) {
-      std::cout << "TEST 1 PASSED: " << test.testReading() << "\n";
-      std::cout << "TEST 2 PASSED: " << test.testOccurence() << "\n";
+  if (1) {
+    std::cout << "TEST 1 PASSED: " << test.testReading() << "\n";
+    std::cout << "TEST 2 PASSED: " << test.testOccurence() << "\n";
+  }
+
+  text.read("../test/test.txt");
+
+  // removes dots, commas, question marks, etc.
+
+  text.removePunctuation();
+
+  for (auto &x : text.getWords()) {
+    // Check if the word is part of the map
+    if (result.count(x)) {
+      // Increment the occurence by one
+      result.find(x)->second++;
+    } else {
+      // if the word is new we insert it to the map
+      result.insert(std::pair<std::string, int>(x, 1));
     }
+  }
 
-    text.read("../test/test.txt");
-
-    // removes dots, commas, question marks, etc.
-
-    text.removePunctuation();
-
-
-    for (auto &x : text.getWords()) {
-      if (result.count(x)) {
-	// Check if the word is part of the map and increment the occurence
-        result.find(x)->second++;
-      } else {
-	// if the word is new we insert it to the map
-        result.insert(std::pair<std::string, int>(x, 1));
-      }
-    }
-
-    // Print the occurence of all words in the map
-    for (auto &x : result) {
-      std::cout << " " << x.second << ": " << x.first << "\n";
-    }
+  // Print the occurence of all words in the map
+  for (auto &x : result) {
+    std::cout << " " << x.second << ": " << x.first << "\n";
+  }
 
     return 0;
 }
