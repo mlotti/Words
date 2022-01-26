3ce9db4e66831e46bd76bbaf4259daf4d7e9d4d4
mlotti <mikko.lotti@cern.ch>
2022-01-26 15:29:52 +0100

    Added Text class to save and modify the input

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # Words
+
+Count the occurrence of each unique word a the file. 
+
+The words are read as a default from test/test.txt file in to a vector and after being cleaned, the words are connected to their occurence counts using a map. 
+
+The program can be compiled with
+
+```
+cd work
+make
+```
+
+and executed with 
+
+```
+./exe
+```
+
+# TO-DO: 
+
+* speed up
+* additional testing
+* Further cleaning of text 
+
+# Future exstensions
+
+* What is the most common word? 
+* What is the amount of words?
+* What is the longest word?
+
---
+++ src/Text.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+
+#include "Text.h"
+
+Text::Text(){
+
+}
+
+Text::~Text(){
+
+}
+
+void Text::read(std::string fileName) {
+
+    std::string word;
+    std::ifstream MyFile(fileName);
+
+    if (!MyFile.is_open()) {
+	std::cout << "File does not exist" << "\n";
+	return;
+    }
+
+    while (MyFile >> word) {
+        words.push_back(word);
+    }
+
+    MyFile.close();
+
+    return;
+}
+
+int Text::countOccurence(std::string word) {
+
+  int count = 0;
+  for (auto &w : words) {
+	if (w == word) {
+	  count++;
+	}
+  }
+  return count;
+}
+
+void Text::removePunctuation(){
+
+  for (auto &word : words) {
+    word.erase(std::remove_if(word.begin(), word.end(), ispunct), word.end());
+  }
+  return;
+}
+
+void Text::lowerCaseWords() {
+
+}
+
